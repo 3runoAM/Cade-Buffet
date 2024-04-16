bc5ec6d81e775dcea5eb84a7d15e96bc275f8773
Bruno Martins <jbrunno17@gmail.com>
2024-04-16 13:55:16 -0300

    Adiciona testes unitários

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum role: { client: 0, owner: 1 }
+  validates :name, presence: true
+  validates :role, inclusion: { in: roles.keys }
 
+  private
 
   def self.role_options
-    roles.keys.map { |r| [I18n.t("activerecord.attributes.user.roles.#{r}"), r]}
+    roles.keys.map { |r| [r, I18n.t("activerecord.attributes.user.roles.#{r}")]}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe "#valid" do
+    it "is invalid with an invalid email" do
+      owner = User.new(name: "Mateus Alves", email: 'email', password: 'password', role: :owner)
+
+      result = owner.valid?
+      expect(result).to eq false
+    end
+
+    it 'is invalid with a non-existent role' do
+      expect {
+        User.new(name: "Mateus Alves", email: 'invalid', password: 'password', role: :adm)
+      }.to raise_error(ArgumentError)
+    end
+
+    it 'should be valid' do
+      owner = User.new(name: "Mateus Alves", email: 'example@example.com', password: 'password', role: :client)
+      result = owner.valid?
+
+      expect(result).to eq true
+    end
+  end
+
+  describe ".role_options" do
+    it 'should translate properly' do
+      role_options = User.role_options
+
+      expect(role_options.first).to eq ["client", "Cliente"]
+      expect(role_options.second).to eq ["owner", "Proprietário de Buffet"]
+    end
+  end
 end

77aec0a2b95c0795d46a6917a05ff2fe9142bfe9
Bruno Martins <jbrunno17@gmail.com>
2024-04-22 14:30:46 -0300

    Implementa verificação de buffet cadastrado na rota de acesso a área do proprietário

--- app/controllers/owner/dashboards_controller.rb
+++ app/controllers/owner/dashboards_controller.rb
@@ -1,4 +1,5 @@
 class Owner::DashboardsController < ApplicationController
+  before_action :ensure_registered_buffet, only: [:index]
   def index
     @buffet = Buffet.find_by(user_id: current_user.id)
     @events = @buffet.events

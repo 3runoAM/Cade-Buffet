13d019553f97df35924aed466d25452b8a63db3c
Bruno Martins <jbrunno17@gmail.com>
2024-05-13 18:45:19 -0300

    Adiciona instâncias de exemplo ao seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,13 @@ buffet.save!
 
 Address.create!(street_name: 'Rua das Palmeiras', neighborhood: 'Centro', house_or_lot_number: '123',
                 state: 'Paraíba', city: 'Patos', zip: '58700-000', buffet: buffet)
-event = Event.create!(name: 'Casamento na Praia', description: 'Um evento maravilhoso à beira-mar com decoração temática e menu personalizado.', buffet: buffet,
+first_event = Event.create!(name: 'Casamento na Praia', description: 'Um evento maravilhoso à beira-mar com decoração temática e menu personalizado.', buffet: buffet,
                             min_guests: 50, max_guests: 200, standard_duration: 600, menu: 'Frutos do Mar',
                             offsite_event: true, offers_alcohol: true, offers_decoration: true,
                             offers_valet_parking: true)
-EventPrice.create!(event: event, standard_price: 5000, extra_guest_price: 100,
+EventPrice.create!(event: first_event, standard_price: 5000, extra_guest_price: 100,
                    extra_hour_price: 200, day_type: :weekday)
-EventPrice.create!(event: event, standard_price: 7000, extra_guest_price: 150,
+EventPrice.create!(event: first_event, standard_price: 7000, extra_guest_price: 150,
                    extra_hour_price: 300, day_type: :weekend)
 
 second_owner = User.create!(name: 'Maria Santos', email: 'maria_santos@example.com',
@@ -33,13 +33,13 @@ second_buffet.save!
 
 Address.create!(street_name: 'Rua das Orquídeas', neighborhood: 'Jardim', house_or_lot_number: '456',
                 state: 'Paraíba', city: 'Patos', zip: '58700-000', buffet: second_buffet)
-event = Event.create!(name: 'Festa Tropical', description: 'Um evento incrível com decoração tropical e menu personalizado.', buffet: second_buffet,
+second_event = Event.create!(name: 'Festa Tropical', description: 'Um evento incrível com decoração tropical e menu personalizado.', buffet: second_buffet,
                       min_guests: 60, max_guests: 250, standard_duration: 720, menu: 'Comida Tropical',
                       offsite_event: true, offers_alcohol: true, offers_decoration: true,
                       offers_valet_parking: true)
-EventPrice.create!(event: event, standard_price: 6000, extra_guest_price: 120,
+EventPrice.create!(event: second_event, standard_price: 6000, extra_guest_price: 120,
                    extra_hour_price: 250, day_type: :weekday)
-EventPrice.create!(event: event, standard_price: 8000, extra_guest_price: 160,
+EventPrice.create!(event: second_event, standard_price: 8000, extra_guest_price: 160,
                    extra_hour_price: 350, day_type: :weekend)
 
 # Terceiro Buffet
@@ -53,13 +53,13 @@ third_buffet.save!
 
 Address.create!(street_name: 'Rua dos Jasmins', neighborhood: 'Bela Vista', house_or_lot_number: '789',
                 state: 'Paraíba', city: 'Patos', zip: '58700-000', buffet: third_buffet)
-event = Event.create!(name: 'Festa da Primavera', description: 'Um evento vibrante com decoração floral e menu sazonal.', buffet: third_buffet,
+third_event = Event.create!(name: 'Festa da Primavera', description: 'Um evento vibrante com decoração floral e menu sazonal.', buffet: third_buffet,
                       min_guests: 70, max_guests: 300, standard_duration: 840, menu: 'Comida Vegetariana',
                       offsite_event: true, offers_alcohol: true, offers_decoration: true,
                       offers_valet_parking: true)
-EventPrice.create!(event: event, standard_price: 7000, extra_guest_price: 140,
+EventPrice.create!(event: third_event, standard_price: 7000, extra_guest_price: 140,
                    extra_hour_price: 280, day_type: :weekday)
-EventPrice.create!(event: event, standard_price: 9000, extra_guest_price: 180,
+EventPrice.create!(event: third_event, standard_price: 9000, extra_guest_price: 180,
                    extra_hour_price: 360, day_type: :weekend)
 
 # Quarto Buffet
@@ -73,13 +73,13 @@ fourth_buffet.save!
 
 Address.create!(street_name: 'Rua das Margaridas', neighborhood: 'Flores', house_or_lot_number: '012',
                 state: 'Paraíba', city: 'Patos', zip: '58700-000', buffet: fourth_buffet)
-event = Event.create!(name: 'Festa da Terra', description: 'Um evento eco-friendly com decoração sustentável e menu orgânico.', buffet: fourth_buffet,
+fourth_event = Event.create!(name: 'Festa da Terra', description: 'Um evento eco-friendly com decoração sustentável e menu orgânico.', buffet: fourth_buffet,
                       min_guests: 80, max_guests: 350, standard_duration: 960, menu: 'Comida Orgânica',
                       offsite_event: true, offers_alcohol: true, offers_decoration: true,
                       offers_valet_parking: true)
-EventPrice.create!(event: event, standard_price: 8000, extra_guest_price: 160,
+EventPrice.create!(event: fourth_event, standard_price: 8000, extra_guest_price: 160,
                    extra_hour_price: 320, day_type: :weekday)
-EventPrice.create!(event: event, standard_price: 10000, extra_guest_price: 200,
+EventPrice.create!(event: fourth_event, standard_price: 10000, extra_guest_price: 200,
                    extra_hour_price: 400, day_type: :weekend)
 
 # Quinto Buffet
@@ -93,57 +93,45 @@ fifth_buffet.save!
 
 Address.create!(street_name: 'Rua dos Lírios', neighborhood: 'Estrela', house_or_lot_number: '345',
                 state: 'Paraíba', city: 'Patos', zip: '58700-000', buffet: fifth_buffet)
-event = Event.create!(name: 'Festa Gourmet', description: 'Um evento elegante com decoração de luxo e menu gourmet.', buffet: fifth_buffet,
+fifth_event = Event.create!(name: 'Festa Gourmet', description: 'Um evento elegante com decoração de luxo e menu gourmet.', buffet: fifth_buffet,
                       min_guests: 90, max_guests: 400, standard_duration: 1080, menu: 'Comida Gourmet',
                       offsite_event: true, offers_alcohol: true, offers_decoration: true,
                       offers_valet_parking: true)
-EventPrice.create!(event: event, standard_price: 9000, extra_guest_price: 180,
+EventPrice.create!(event: fifth_event, standard_price: 9000, extra_guest_price: 180,
                    extra_hour_price: 360, day_type: :weekday)
-EventPrice.create!(event: event, standard_price: 11000, extra_guest_price: 220,
+EventPrice.create!(event: fifth_event, standard_price: 11000, extra_guest_price: 220,
                    extra_hour_price: 440, day_type: :weekend)
 
-# first_owner = User.create!(name: 'Fabrício', email: 'first_owner@example.com',
-#                            password: 'password1', role: :owner)
-# first_buffet = Buffet.new(user: first_owner, brand_name: 'Buffet 1', company_name: 'Company 1',
-#                           crn: '31.602.413/0001-70', phone: '111-111-1111', email: 'buffet1@example.com',
-#                           description: 'Description 1')
-# first_buffet.payment_methods << p1
-# first_buffet.payment_methods << p2
-# first_buffet.payment_methods << p3
-# first_buffet.payment_methods << p4
-# first_buffet.payment_methods << p5
-# first_buffet.save!
-#
-# Address.create!(street_name: 'Street 1', neighborhood: 'Neighborhood 1', house_or_lot_number: '1',
-#                 state: 'State 1', city: 'City 1', zip: '11111', buffet: first_buffet)
-# first_event = Event.create!(name: 'Event 1', description: 'Description 1', buffet: first_buffet,
-#                             min_guests: 10, max_guests: 100, standard_duration: 300, menu: 'Menu 1',
-#                             offsite_event: true, offers_alcohol: true, offers_decoration: true,
-#                             offers_valet_parking: true)
-# EventPrice.create!(event: first_event, standard_price: 1000, extra_guest_price: 100,
-#                                           extra_hour_price: 50, day_type: :weekday)
-# EventPrice.create!(event: first_event, standard_price: 1500, extra_guest_price: 150,
-#                                          extra_hour_price: 75, day_type: :weekend)
-#
-# second_owner = User.create!(name: 'Carlos', email: 'second_owner@example.com',
-#                             password: 'password2', role: :owner)
-# second_buffet = Buffet.new(user: second_owner, brand_name: 'Buffet 2', company_name: 'Company 2',
-#                            crn: '18.312.384/0001-43', phone: '222-222-2222', email: 'buffet2@example.com',
-#                            description: 'Description 2')
-# second_buffet.payment_methods << p1
-# second_buffet.payment_methods << p2
-# second_buffet.payment_methods << p3
-# second_buffet.payment_methods << p4
-# second_buffet.payment_methods << p5
-# second_buffet.save!
-#
-# Address.create!(street_name: 'Street 2', neighborhood: 'Neighborhood 2', house_or_lot_number: '2',
-#                 state: 'State 2', city: 'City 2', zip: '22222', buffet: second_buffet)
-# second_event = Event.create!(name: 'Event 2', description: 'Description 2', buffet: second_buffet,
-#                              min_guests: 10, max_guests: 100, standard_duration: 600, menu: 'Menu 2',
-#                              offsite_event: false, offers_alcohol: true, offers_decoration: true,
-#                              offers_valet_parking: true)
-# EventPrice.create!(event: second_event, standard_price: 2000, extra_guest_price: 200,
-#                                            extra_hour_price: 100, day_type: :weekday)
-# EventPrice.create!(event: second_event, standard_price: 2500, extra_guest_price: 250,
-#                                           extra_hour_price: 125, day_type: :weekend)
+client1 = User.create!(name: 'João Pedro', email: 'joao_pedro@example.com', password: 'password1', role: :client,
+                       cpf: '182.342.840-10')
+client2 = User.create!(name: 'Caio Martins', email: 'caio_martins@example.com', password: 'password2', role: :client,
+                       cpf: "808.677.250-01")
+client3 = User.create!(name: 'Felipe Souza', email: 'felipe_souza@example.com', password: 'password3', role: :client,
+                       cpf: "578.611.620-44")
+client4 = User.create!(name: 'Juliana Thaís', email: 'juliana_thais@example.com', password: 'password4', role: :client,
+                       cpf: "508.563.400-49")
+client5 = User.create!(name: 'Roberta Silva', email: 'roberta_silva@example.com', password: 'password5', role: :client,
+                       cpf: "639.137.330-27")
+client6 = User.create!(name: 'Brendon Costa', email: 'brendon_costa@example.com', password: 'password6', role: :client,
+                       cpf: "831.434.220-33")
+
+Order.create!(user: client1, event: first_event, buffet: buffet, total_guests: 100,
+              address: "Rua das Flores, Bairro Jardim, 123", event_date: 1.month.from_now.next_weekday, price: 5000,
+              additional_info: "Informações adicionais do pedido 1 para o evento 1 em Buffet 1 em 1 mês")
+
+Order.create!(user: client2, event: second_event, buffet: second_buffet, total_guests: 100,
+              address: "Avenida do Sol, Bairro Centro, 456", event_date: 2.months.from_now.next_weekday, price: 7500,
+              additional_info: "Informações adicionais do pedido 2 para o evento 2 em Buffet 2 em 2 meses")
+
+Order.create!(user: client3, event: third_event, buffet: third_buffet, total_guests: 100,
+              address: "Travessa da Lua, Bairro Estrela, 789", event_date: 3.months.from_now.next_weekday, price: 10000,
+              additional_info: "Informações adicionais do pedido 3 para o evento 3 em Buffet 3 em 3 meses")
+
+Order.create!(user: client4, event: fourth_event, buffet: fourth_buffet, total_guests: 100,
+              address: "Praça das Nuvens, Bairro Céu, 101112", event_date: 4.months.from_now.next_weekday, price: 12500,
+              additional_info: "Informações adicionais do pedido 4 para o evento 4 em Buffet 4 em 4 meses")
+
+Order.create!(user: client5, event: fifth_event, buffet: fifth_buffet, total_guests: 100,
+              address: "Alameda dos Cometas, Bairro Universo, 131415", event_date: 5.months.from_now.next_weekday, price: 15000,
+              additional_info: "Informações adicionais do pedido 5 para o evento 5 em Buffet 5 em 5 meses")
+

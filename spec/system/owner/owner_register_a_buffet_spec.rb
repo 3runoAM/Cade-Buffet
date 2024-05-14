5536228345f4f931a123ecca14df69fb6b6137c3
Bruno Martins <jbrunno17@gmail.com>
2024-05-14 19:27:20 -0300

    Finaliza refatoração dos testes, excluindo algumas macros de support utilizadas até então[

--- spec/system/owner/owner_register_a_buffet_spec.rb
+++ spec/system/owner/owner_register_a_buffet_spec.rb
@@ -8,7 +8,23 @@ describe "Buffet owner register a buffet" do
 
     visit root_path
     sign_up
-    register_buffet
+
+    fill_in 'Nome fantasia', with: 'Buffet do Mateus'
+    fill_in 'Razão social', with: 'M.A. LTDA'
+    fill_in 'CNPJ', with: '23.261.499/0001-96'
+    fill_in 'Telefone', with: '(83)95555-5555'
+    fill_in 'Email', with: 'mateus@example.com'
+    check 'Pix'
+    check 'Cartão de débito'
+    fill_in 'Rua', with: 'Rua dos planetas'
+    fill_in 'Número ou lote', with: '963'
+    fill_in 'Bairro', with: 'Sistema solar'
+    fill_in 'Cidade', with: 'Via lactea'
+    fill_in 'Estado', with: 'Universo'
+    fill_in 'Cep', with: '00000-00'
+    fill_in 'Descrição', with: 'Buffet infantil'
+
+    click_on 'Criar Buffet'
 
     expect(page).to have_content 'Buffet do Mateus'
     expect(page).to have_content 'M.A. LTDA'
